282d91fa4d9f628acb9e084781189ea95fdf6b30
Julieta Preliasco Benitez <preliascojulieta@gmail.com>
2024-08-27 17:28:41 -0300

    LL04: Create Landing Banner

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-
 interface ButtonProps {
   onClick?: () => void;
-  variant?: "PRIMARY" | "SECONDARY" | "DISABLED";
+  variant?: "PRIMARY_NAV" | "SECONDARY_NAV" | "SECONDARY";
   type?: "submit" | "reset" | "button" | undefined;
   disabled?: boolean;
   label: string;
@@ -12,20 +10,30 @@ const Button = ({ onClick, variant, type, disabled, label }: ButtonProps) => {
   let variantClass;
   switch (variant) {
     case "SECONDARY":
-      variantClass = "border border-1 border-custom-gray hover:bg-custom-gray";
+      variantClass =
+        "border border-1 border-custom-gray hover:bg-custom-gray text-custom-black";
+      break;
+    case "PRIMARY_NAV":
+      variantClass = "text-white";
+      break;
+    case "SECONDARY_NAV":
+      variantClass = "text-custom-black";
       break;
     default:
-      variantClass = "bg-custom-yellow hover:bg-[#dab431]";
+      variantClass = "bg-custom-yellow hover:bg-[#dab431] text-custom-black";
   }
 
   return (
-    <button
-      onClick={onClick}
-      disabled={disabled}
-      className={`${disabled ? "bg-custom-gray" : variantClass} font-lato text-custom-black uppercase font-bold text-[16px] leading-[19.2px] px-[24px] py-[10px] rounded-md`}
-    >
-      {label}
-    </button>
+    <div className="w-fit h-fit">
+      <button
+        type={type}
+        onClick={onClick}
+        disabled={disabled}
+        className={`${disabled ? "bg-custom-gray" : variantClass} font-lato uppercase font-bold text-[16px] leading-[19.2px] px-[24px] py-[10px] rounded-md`}
+      >
+        {label}
+      </button>
+    </div>
   );
 };
 
