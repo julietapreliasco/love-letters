e91cca9ad032107aa8682adc74af731a578d9588
Julieta Preliasco Benitez <preliascojulieta@gmail.com>
2024-09-06 10:42:23 -0300

    LL-fix: Fix projects section

--- src/components/landing/ProjectsSection.tsx
+++ src/components/landing/ProjectsSection.tsx
@@ -1,19 +1,31 @@
+'use client';
 import { LandingSectionType } from '@/contentful/landingSections';
 import Button from '../ui/Button';
 import Card from '../ui/Card';
-import { CardType, fetchCards } from '@/contentful/cards';
+import { CardType } from '@/contentful/cards';
+import Image from 'next/image';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 interface ProjectsSectionClientProps {
   projectData: LandingSectionType;
 }
 
-const ProjectsSection: React.FC<ProjectsSectionClientProps> = async ({
+const ProjectsSection: React.FC<ProjectsSectionClientProps> = ({
   projectData,
 }) => {
+  const container = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start start', 'end end'],
+  });
+
   const projectCards = projectData.cards;
 
+  const scale = useTransform(scrollYProgress, [0, 1], [0.9, 0.75]);
+
   return (
-    <section>
+    <section ref={container} style={{ minHeight: '100vh' }}>
       <div className="mx-[20px] mb-[20px] mt-[80px] flex h-auto flex-row justify-between md:mx-[60px] md:mb-[40px]">
         <h1 className="font-playfair-display text-[32px] font-bold md:text-[64px]">
           {projectData.title}
@@ -30,17 +42,21 @@ const ProjectsSection: React.FC<ProjectsSectionClientProps> = async ({
       <ul>
         {projectCards &&
           projectCards.map((project: CardType, index) => (
-            <li key={index} className="relative">
-              <div
-                className="sticky top-0 z-10 h-[110vh] bg-black"
-                style={{
-                  backgroundImage: `url(${project?.image?.src})`,
-                  backgroundSize: 'cover',
-                  backgroundPosition: 'center',
-                  backgroundAttachment: 'fixed',
-                }}
-              >
-                <div className="absolute right-0 top-1/4 m-6">
+            <li
+              key={index}
+              className={`sticky top-28 z-10 m-10 h-[80vh] items-center justify-between overflow-hidden rounded-2xl ${index === 0 || index === 2 ? 'bg-custom-yellow' : 'bg-custom-gray'}`}
+            >
+              <div className="flex">
+                <motion.div className="left-0" style={{ scale: scale }}>
+                  <Image
+                    src={project.image?.src!}
+                    alt={project.image?.alt!}
+                    layout="intrinsic"
+                    width={2152}
+                    height={1536}
+                  />
+                </motion.div>
+                <div className="relative top-20 flex w-full flex-col items-end justify-center p-10">
                   <Card
                     buttonLabel="about me"
                     card={{
