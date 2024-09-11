133cec606a42458881a5075ec0ff57d3749fd1fc
Paula Rinaldi <paurinaldi95@gmail.com>
2024-09-11 16:29:08 -0300

    LL13: style fix, blob animation fix

--- src/components/about-me/AboutMe.tsx
+++ src/components/about-me/AboutMe.tsx
@@ -4,6 +4,9 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { BLOCKS } from '@contentful/rich-text-types';
 import Image from 'next/image';
 import { motion, useScroll, useTransform } from 'framer-motion';
+import LottieAnimation from '../ui/LottieAnimation';
+import animationData from '../../../public/about-us-animation.json';
+import { useState } from 'react';
 
 interface AboutMeProps {
   data: PageType;
@@ -11,54 +14,103 @@ interface AboutMeProps {
 
 const AboutMe = ({ data }: AboutMeProps) => {
   const { description, bannerTitle, bannerImg } = data;
+  const [hoveredIndices, setHoveredIndices] = useState<number[]>([]);
+
+  const handleMouseEnter = (index: number) => {
+    setHoveredIndices((prev) => [...prev, index]);
+  };
+
+  const handleMouseLeave = (index: number) => {
+    setHoveredIndices((prev) => prev.filter((i) => i !== index));
+  };
+
+  let assetIndex = 0;
 
   const renderOptions = {
     renderNode: {
       [BLOCKS.PARAGRAPH]: (node: any, children: React.ReactNode) => (
-        <p key={node.data?.target?.sys?.id || Math.random()} className="mb-4 font-lato lg:text-xl xl:text-2xl">
+        <p
+          key={node.data?.target?.sys?.id || Math.random()}
+          className="text-justify font-lato lg:text-lg xl:text-xl"
+        >
           {children}
         </p>
       ),
       [BLOCKS.EMBEDDED_ASSET]: (node: any) => {
         const { file, title } = node.data.target.fields;
         const imageUrl = 'https:' + file.url;
+
+        const isOdd = assetIndex % 2 === 0;
+        const currentAssetIndex = assetIndex;
+        assetIndex += 1;
+
+        const isHovered = hoveredIndices.includes(currentAssetIndex);
+
         return (
-          <img
-            key={file.url}
-            src={imageUrl}
-            width={file.details.image.width}
-            height={file.details.image.height}
-            alt={title || ''}
-            className="max-h-[500px] w-full object-cover object-top md:max-h-[437px] md:min-w-[40vw]"
-          />
+          <div
+            onMouseEnter={() => handleMouseEnter(currentAssetIndex)}
+            onMouseLeave={() => handleMouseLeave(currentAssetIndex)}
+            className={`relative flex h-[300px] w-[300px] items-center justify-center md:h-[350px] md:w-[350px] lg:h-[400px] lg:w-[400px] xl:h-[500px] xl:w-[500px]`}
+          >
+            <div className="absolute inset-0 flex items-center justify-center">
+              <LottieAnimation
+                autoplay={false}
+                hover={isHovered}
+                animationData={animationData}
+              />
+            </div>
+
+            <div className="relative flex h-[60%] w-[60%] items-center justify-center">
+              <img
+                key={file.url}
+                src={imageUrl}
+                alt={title || ''}
+                className="h-full w-full rounded-full object-cover object-top"
+              />
+            </div>
+          </div>
         );
       },
     },
   };
 
-  const contentComponents = documentToReactComponents(description!, renderOptions);
-  const contentArray = Array.isArray(contentComponents) ? contentComponents : [contentComponents];
+  const contentComponents = documentToReactComponents(
+    description!,
+    renderOptions
+  );
+  const contentArray = Array.isArray(contentComponents)
+    ? contentComponents
+    : [contentComponents];
 
   const groupedContent: JSX.Element[] = [];
   let i = 0;
-  while (i <= contentArray.length) {
-    const currentImage = contentArray[i]?.type === 'img' ? contentArray[i] : null;
-    const nextParagraphs = contentArray.slice(i + 1, i + 3).filter(item => item.type === 'p');
+  while (i < contentArray.length) {
+    const currentImage =
+      contentArray[i]?.type === 'div' ? contentArray[i] : null;
+    const nextParagraphs = contentArray
+      .slice(i + 1, i + 3)
+      .filter((item) => item.type === 'p');
 
     if (currentImage && nextParagraphs.length === 2) {
       const isOdd = groupedContent.length % 2 === 0;
       groupedContent.push(
         <div
           key={i}
-          className={`flex flex-col gap-5 md:flex-row md:justify-between ${!isOdd ? 'md:flex-row-reverse' : ''}`}
+          className={`flex flex-col md:flex-row ${
+            !isOdd
+              ? 'md:ml-10 md:flex-row-reverse lg:ml-20'
+              : 'md:mr-10 lg:mr-20'
+          } `}
         >
-          <div className="self-center">{currentImage}</div>
-          <div className={`py-5 md:max-w-[50%] ${isOdd ? 'md:ml-10' : 'md:mr-10'}`}>
-            {nextParagraphs}
+          <div className="flex flex-1 items-center justify-center">
+            {currentImage}
+          </div>
+          <div className="flex flex-1 items-center justify-center px-10 sm:px-20 md:p-0">
+            <div className="flex flex-col gap-4">{nextParagraphs}</div>
           </div>
         </div>
       );
-      i += 3
+      i += 3;
     } else {
       i++;
     }
@@ -71,7 +123,10 @@ const AboutMe = ({ data }: AboutMeProps) => {
     <div className="flex flex-col bg-custom-lighter-gray">
       <div className="relative flex w-full flex-col items-center justify-center overflow-hidden bg-custom-gray md:h-screen md:flex-row">
         {bannerImg && (
-          <motion.div className="z-0 flex items-center justify-center pt-20 md:w-[60%] md:pt-10" style={{ scale }}>
+          <motion.div
+            className="z-0 flex items-center justify-center pt-20 md:w-[60%] md:pt-10"
+            style={{ scale }}
+          >
             <Image
               src={bannerImg.src}
               alt="Love Letters Home Banner"
@@ -86,9 +141,7 @@ const AboutMe = ({ data }: AboutMeProps) => {
           <span>{bannerTitle}</span>
         </div>
       </div>
-      <div className="space-y-10 px-6 py-10 md:px-24 md:py-20">
-        {groupedContent}
-      </div>
+      <div className="px-6 py-10 lg:px-16 lg:py-10">{groupedContent}</div>
     </div>
   );
 };
