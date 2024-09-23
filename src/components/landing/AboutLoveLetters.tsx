27a569d5f98812a33b4ebd18efc50e0b852095bc
Julieta Preliasco Benitez <preliascojulieta@gmail.com>
2024-09-23 10:19:09 -0300

    LL-fix: About Love Letters Animation

--- src/components/landing/AboutLoveLetters.tsx
+++ src/components/landing/AboutLoveLetters.tsx
@@ -1,6 +1,8 @@
 import { LandingSectionType } from '@/contentful/landingSections';
 import AboutLoveLettersSVG from '../ui/AboutLoveLettersSVG';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import LottieAnimation from '../ui/LottieAnimation';
+import animationData from '../../../public/about-love-letters-animation.json';
 
 interface AboutLoveLettersProps {
   aboutLoveLettersData: LandingSectionType;
@@ -14,7 +16,11 @@ const AboutLoveLetters: React.FC<AboutLoveLettersProps> = ({
   return (
     <section className="flex flex-col items-center gap-14 bg-custom-lighter-gray px-[40px] py-20 md:px-[60px] lg:flex-row xl:gap-28">
       <div className="flex flex-1 justify-center">
-        <AboutLoveLettersSVG />
+        <LottieAnimation
+          autoplay={true}
+          hover={false}
+          animationData={animationData}
+        />
       </div>
       <div className="flex flex-1 flex-col xl:gap-5">
         <p className="mb-5 text-center font-playfair-display text-2xl font-bold text-custom-black md:text-5xl lg:text-start 2xl:text-6xl">
