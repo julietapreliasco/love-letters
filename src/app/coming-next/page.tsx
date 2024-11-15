06e7539e6b217d0a32b854fc7311c8237b99790a
Paula Rinaldi <paurinaldi95@gmail.com>
2024-11-15 10:29:37 -0300

    LL-36: created coming next page

---
+++ src/app/coming-next/page.tsx
@@ -0,0 +1,46 @@
+import Button from '@/components/ui/Button';
+import { fetchPageByName } from '@/contentful/pages';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS } from '@contentful/rich-text-types';
+
+const Places = async () => {
+  const page = await fetchPageByName({
+    preview: false,
+    page: 'Coming next',
+  });
+
+  const options = {
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (_: any, children: React.ReactNode) => (
+        <p className="mb-4 text-justify">{children}</p>
+      ),
+    },
+  };
+
+  return (
+    <div className="flex flex-col items-center gap-10 px-8 py-28 md:py-[150px] xl:px-[160px]">
+      <h2 className="text-center font-futura text-xl font-medium uppercase leading-normal tracking-wider md:mb-10 md:text-3xl md:leading-normal lg:text-4xl lg:leading-normal">
+        {page?.bannerTitle}
+      </h2>
+      {page?.richText && (
+        <div className="font-lato xl:text-lg xl:leading-normal">
+          {documentToReactComponents(page.richText, options)}
+        </div>
+      )}
+      {/* <div className="flex flex-col items-center gap-10 rounded-xl border-2 border-custom-black bg-custom-lighter-gray p-10"> */}
+      <Button
+        className="md:self-start"
+        label="donate"
+        linkTo="https://buy.stripe.com/28o14adWR9BX5Lq5kl"
+      />
+      {page?.richTextTwo && (
+        <div className="text-justify font-lato text-sm leading-normal md:self-start xl:text-base xl:leading-normal">
+          {documentToReactComponents(page.richTextTwo, options)}
+        </div>
+      )}
+      {/* </div> */}
+    </div>
+  );
+};
+
+export default Places;
