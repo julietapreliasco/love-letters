0d63117ced8b91fe9364d00bf2372b11787126b2
Julieta Preliasco Benitez <preliascojulieta@gmail.com>
2024-11-18 13:50:01 -0300

    LL-32: Landing updates

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -15,13 +15,36 @@ const Footer = async () => {
 
   return (
     <footer className="w-full bg-custom-lighter-gray px-[35px] py-[46px] md:px-[60px] md:py-[46px]">
-      <div className="flex flex-col items-center justify-between gap-10 border-b border-custom-black pb-5 md:flex-row">
+      <div className="flex flex-col items-center justify-between gap-10 border-b border-custom-black pb-10 md:flex-row">
         <FooterLogo className="h-auto w-[189px]" />
-        <p className="max-w-[480px] self-center text-center font-futura text-lg leading-normal tracking-wider text-custom-black md:max-w-[680px] lg:text-xl lg:leading-normal 2xl:text-2xl 2xl:leading-normal">
-          {data?.description}
-        </p>
+        <div className="grid grid-cols-2 gap-3 md:grid-cols-3 md:items-center lg:flex lg:flex-row">
+          <Button label="Places" linkTo="/places" variant={'SECONDARY_NAV'} />
+          <Button
+            label="Partners"
+            linkTo="/partners"
+            variant={'SECONDARY_NAV'}
+          />
+          <Button label="Press" linkTo="/press" variant={'SECONDARY_NAV'} />
+          <Button label="Academy" linkTo="/academy" variant={'SECONDARY_NAV'} />
+          <Button label="About" linkTo="/about-me" variant={'SECONDARY_NAV'} />
+          <Button
+            label="Speaking"
+            linkTo="/speaking"
+            variant={'SECONDARY_NAV'}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-between gap-11 pt-8 md:flex-row">
+        <div>
+          <span className="w-full pb-[10px] pt-10 text-center md:hidden">
+            Copyright &copy; 2024. All rights reserved.
+          </span>
+          <span className="mt-8 hidden self-center md:block md:text-sm xl:mt-0 xl:text-base">
+            Copyright &copy; 2024. All rights reserved.
+          </span>
+        </div>
         <div className="flex flex-col gap-[10px] md:self-center">
-          <span className="mr-4 text-center font-lato md:text-start lg:text-xl">
+          <span className="text-center font-lato md:text-start lg:text-xl">
             Follow us:
           </span>
           <div className="flex space-x-4">
@@ -42,30 +65,6 @@ const Footer = async () => {
             })}
           </div>
         </div>
-      </div>
-      <div className="flex flex-col items-center justify-between pt-5 xl:flex-row-reverse">
-        <div className="flex flex-col items-center pb-5 md:flex-row md:p-0">
-          <Button label="Places" linkTo="/places" variant={'SECONDARY_NAV'} />
-          <Button
-            label="Partners"
-            linkTo="/partners"
-            variant={'SECONDARY_NAV'}
-          />
-          <Button label="Press" linkTo="/press" variant={'SECONDARY_NAV'} />
-          <Button label="Academy" linkTo="/academy" variant={'SECONDARY_NAV'} />
-          <Button label="About" linkTo="/about-me" variant={'SECONDARY_NAV'} />
-          <Button
-            label="Speaking"
-            linkTo="/speaking"
-            variant={'SECONDARY_NAV'}
-          />
-        </div>
-        <span className="w-full border-t border-custom-black pb-[10px] pt-10 text-center md:hidden">
-          Copyright &copy; 2024. All rights reserved.
-        </span>
-        <span className="mt-8 hidden self-center md:block md:text-sm xl:mt-0 xl:text-base">
-          Copyright &copy; 2024. All rights reserved.
-        </span>
       </div>
     </footer>
   );
